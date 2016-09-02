0293d41080fb925e1955fa6f8af2fb2356bbda7c
Ivan Rep <ivan.rep@undabot.com>
2016-09-02 16:49:50 +0200

    tvOS target

---
+++ DVRtvOS/DVRtvOS.h
@@ -0,0 +1,19 @@
+//
+//  DVRtvOS.h
+//  DVRtvOS
+//
+//  Created by Undabot Rep on 02/09/16.
+//  Copyright © 2016 Venmo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DVRtvOS.
+FOUNDATION_EXPORT double DVRtvOSVersionNumber;
+
+//! Project version string for DVRtvOS.
+FOUNDATION_EXPORT const unsigned char DVRtvOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DVRtvOS/PublicHeader.h>
+
+
